cabd242ea2498615b4f929e14d0b93e015aa65d1
wenhao <840346763@qq.com>
2020-07-07 14:35:18 +0800

    域名

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,10 +5,11 @@ use Illuminate\Routing\Router;
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
-    'as'            => config('admin.route.prefix') . '.',
+    'domain' => config('admin.route.domain'),
+//    'prefix'        => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
+    'as' => config('admin.route.prefix') . '.',
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
