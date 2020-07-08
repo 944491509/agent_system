16982f37e589854ef466290e0de3c8e739ec13e8
liuyang <944491509@qq.com>
2020-07-08 13:31:18 +0800

    维护车辆

--- app/Admin/Controllers/District/AutomobileController.php
+++ app/Admin/Controllers/District/AutomobileController.php
@@ -34,7 +34,6 @@ class AutomobileController extends AdminController
         });
         $grid->column('bought_company', __('Bought company'));
         $grid->column('car_owner', __('Car owner'));
-        $grid->column('city.name', __('City'));
         $grid->column('stand.name', __('Stand'));
         $grid->column('driver.name', __('Driver'));
         $grid->column('nature', __('Nature'))->display(function () {
@@ -77,11 +76,7 @@ class AutomobileController extends AdminController
         $form->text('engine_num', __('Engine num'));
         $form->text('vin', __('Vin'));
         $form->text('loads', __('Loads'));
-        $form->select('city_id', __('City'))->options($area)
-            ->load('stand_id','/api/stand/get-city-stand','id', 'name');
-        $form->select('stand_id', __('Stand'))->options(function ($id) {
-            return AreaStand::where('id', $id)->pluck('name', 'id'); // 回显
-        })->load('user_id', '/api/stand/get-driver-stand', 'id', 'name');
+        $form->select('stand_id', __('Stand'))->options()->load('user_id', '/api/stand/get-driver-stand', 'id', 'name');
         $form->select('user_id', __('Driver'))->options(function ($id) {
             return User::where('id', $id)->pluck('name', 'id'); // 回显
         });
